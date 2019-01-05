38aa9b93cc66fc3e10fa33bc3fd2ff26089538dc
antoineQue <antoine.queva@orange.fr>
2019-01-05 23:11:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,41 +3,56 @@
 import sha3 as sha3	
 import diffiehellman as dh
 import idea_display as idead
+import os
+import certif as cert
 
 
 while 1:
+
 	print('\n')
 	print("****	MENU PRINCIPAL	****")
 	print("Souhaitez vous:\n")
-	print("	Générer une clé publique / privée (Press 1) \n")
-	print("	Authentifier une clé publique / un certificat (Press 2)\n")
-	print("	Partager une clé secrète (Press 3)\n")
-	print("	Utiliser une clé secrète pour chiffrer un message (et le signer) (Press 4)\n")
-	print("	Déchiffrer un message et vérifier la signature (Press 5)\n")
-	print("	Utiliser hashage SHA3 (Press 6)\n")
-	print("	La totale : THE FULL MONTY (Press 7)\n")
-	print("	Quitter (Press 8)\n")
-	print("choix:")
-	choix = input()
+	print("	1- Générer une paire de clé publique / privée\n")
+	print("	2- Authentifier une clé publique / un certificat\n")
+	print("	3- Générer une clé secrète partagée\n")
+	print("	4- Utiliser une clé secrète pour chiffrer un message (et le signer) \n")
+	print("	5- Déchiffrer un message et vérifier la signature\n")
+	print("	6- Utiliser hashage SHA3\n")
+	print("	7- La totale : THE FULL MONTY\n")
+	print("	8- Quitter\n")
+	choix = input("choix:")
 
 	if choix == "1":
-		#generer_cle()
-		print("Pas encore implémenter")
+		print("1- Générer clés RSA\n")
+		print("2- Générer clés DiffieHellman\n")
+		algo = input("choix:")	
+		
+		if algo == "1":
+			length = int(input("taille: "))
+			pub_key , priv_key = cert.gen_key(length)
+		elif algo == "2":
+			A, alpha, p = dh.gen_dh_key()
+	
 	if choix == "2":
-		#authentifier()
-		print("Pas encore implémenter")
+		pub_key_utt , priv_key_utt = cert.gen_key(2048)
+	
 	if choix == "3":
 		dh.main("none")
+	
 	if choix == "4":
 		idead.main("c","none")
+	
 	if choix == "5":
 		idead.main("d","none")
+	
 	if choix == "6":
 		sha3.main("none","none")
+	
 	if choix == "7":
 		key = dh.main('fc')
 		print("-- Decryption --\n")
 		idead.main("fd",key)
+	
 	if choix == "8":
 		break
 
